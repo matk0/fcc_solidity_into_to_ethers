c1733e9bc7c95307d47f56799acbab319adf5e0a
Matej Lukasik <mat@journalius.com>
2022-05-26 18:46:33 -0600

    Write deploy.js that deploys the smart contracts.

---
+++ deploy.js
@@ -0,0 +1,28 @@
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "cf7e4ba7a5a07536e0bd194280a393b05f8f795981ad43222d8ed9658d08cee2",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait ...");
+  const contract = await contractFactory.deploy();
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
